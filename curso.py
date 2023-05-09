60cadaa62babce069b5e589f7f7d8bd0336dc245
Paulo César coronado <paulocoronado@udistrital.edu.co>
2023-05-08 21:22:12 -0500

    first commit

---
+++ curso.py
@@ -0,0 +1,13 @@
+# Taller No 1: Utilizando python, pedir un número al usuairo y:
+
+# 1. Determinar si el número es primo
+# 2. Determinar si el número es par
+# 3. Imprimir los números enteros entre -n y n siendo n el número ingresado por el usario
+# 4. Imprimir un verso que utilice dicho número si el número está entre 0 y 10
+# 5. Imprimir el número ingresado en letras 989 --> novecientos ochenta y nueve
+# 6. Buscar una palabra en inglés que tenga la cantidad de dígitos de n si n es menor a 7 y mayor a 0
+# 7. Calcular la cantidad de segundo transcurridos desde una fecha aleatoria y dicha fecha + n
+# 8. Calcular el factorial de n si n es menor a 12
+# Crear un repositorio con las diferentes funciones en python
+# Utilizar chat gpt o github copilot (si usan otra mencionarla)
+
